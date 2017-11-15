40fb5a680d52e2fee47077f67e4c2fded35cc958
dearstellar <daintyzizzie@yahoo.com>
2017-11-15 10:13:03 +0100

    Add files via upload

---
+++ snapi.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function ($) {
+
+    // change this when on live server
+    var apiUrl = "http://localhost:5555/register";
+    $("#registerForm").submit(function (event) {
+
+        
+        event.preventDefault();
+        $('#statusMsg').html(' ');
+        statusMsg = "";
+        statusMsg += "<p>Saving details...</p>";
+        $('#statusMsg').html(statusMsg);
+        var publicKey = $('#publicKey').val();
+        var email = $('#email').val();
+        var twitterHandle = $('#twitterHandle').val();
+        var callbackUrl = $('#callbackUrl').val();
+
+        var registerData = {
+            'publick_key': publicKey,
+            'email': email,
+            'twitter_handle': twitterHandle,
+            'callbackurl': callbackUrl
+        };
+
+        console.log(registerData);
+        // Send the data using post
+        var posting = $.post(apiUrl, registerData);
+
+        // Put the results in a div
+        posting.done(function (data) {
+            console.log(data);
+            statusMsg = "<p>You are subscribed</p>";
+            $('#statusMsg').html(statusMsg);
+        });
+
+        posting.fail(function () {
+            statusMsg = "<p>Subscription Error</p>";
+            $('#statusMsg').html(statusMsg);
+        })
+
+
+    });
+
+
+});
